07124a7f15e32bd617b7ba67af840ce5798581a4
yash <yash.shatilov@gmail.com>
2023-04-24 15:39:09 +0300

    fix replace env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 ROOT_DIR=/app
-FILE=$ROOT_DIR'/src/preferenses.js'
 
 echo "Replacing env constants in JS"
-sed -i 's|serverURL|'${SERVER_URL}'|g' $FILE
+for file in $ROOT_DIR/dist/assets/*.js;
+do
+  echo "Processing $file ...";
+
+  sed -i 's|VUE_SERVER_URL|'${SERVER_URL}'|g' $file
+
+done
 
 http-server dist
