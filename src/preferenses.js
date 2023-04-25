e86d5dcc175491770c1f5b8bdbe901538349513f
artempaskevichyan <qeeweewee@gmail.com>
2023-04-25 13:52:10 +0300

    Added https to server url

--- src/preferenses.js
+++ src/preferenses.js
@@ -1,2 +1,2 @@
-export const serverURL = 'VUE_SERVER_URL'
-// export const serverURL = 'https://test.botfood.ru'
+export const serverURL = 'https://VUE_SERVER_URL'
+// export const serverURL = 'https://api.tochka-remonta.site'
