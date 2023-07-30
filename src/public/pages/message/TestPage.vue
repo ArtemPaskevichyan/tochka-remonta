02b81f33624626b982b17a47cbe25e659a6b42c0
artempaskevichyan <qeeweewee@gmail.com>
2023-07-30 19:26:28 +0300

    added test mage and chat iframe

--- src/public/pages/message/TestPage.vue
+++ src/public/pages/message/TestPage.vue
@@ -1,14 +1,14 @@
 <template>
-  <UIHeader></UIHeader>
-
-  CHAT
-  <iframe src="https://chat.tochka-remonta.site?layout=embedded" frameborder="0">
-
-  </iframe>
+  <div class="headerPage iframePage">
+    <UIHeader></UIHeader>
+    <iframe id="chat" src="https://chat.tochka-remonta.site?layout=embedded" frameborder="0">
+    </iframe>
+  </div>
 </template>
 
 <script>
 import UIHeader from '@/components/Header/UIHeader.vue'
+import axios from 'axios'
 
 export default {
   components: {
@@ -16,6 +16,28 @@ export default {
   },
   data() {
     return {}
+  },
+  methods: {
+    async onMounted() {
+      axios.post("https://chat.tochka-remonta.site/api/v1/login", {
+        user: "new-user",
+        password: "new-users-passw0rd",
+      }, {
+        headers: {
+          "Access-Control-Allow-Origin": "https://chat.tochka-remonta.site",
+          "Access-Control-Allow-Credentials": "true",
+        }
+      })
+      .then((response) => {
+        console.log("CHAT RESPONSE", response)
+      })
+      .catch((error) => {
+        console.log("CHAT ERROR", error)
+      })
+    }
+  },
+  mounted() {
+    this.onMounted()
   }
 }
 </script>
